6026958ec4f5417d9a51db2e831d57885537ffd0
AntoineCrypt0X <antoine.poirier1995@gmail.com>
2024-09-03 16:23:39 +0200

    Add files via upload

---
+++ Tokens/fork.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AirDrop is Ownable {
+
+    // Contract address of the old token
+    IERC20 public immutable oldToken;
+
+    // Contract address of the new token
+    IERC20 public immutable newToken;
+
+    uint256 public coefficient;
+
+    constructor(address _oldToken, address _newToken, uint256 _coefficient) Ownable(msg.sender) {
+        oldToken = IERC20(_oldToken);
+        newToken = IERC20(_newToken);
+        coefficient=_coefficient;
+    }
+
+    function fork(uint256 amount) public returns (bool) {
+        oldToken.transferFrom(msg.sender, address(this), amount);
+        newToken.transfer(msg.sender,amount*coefficient);
+        return true;
+    }
+
+    function return_To_Owner(IERC20 token, uint256 _amount)  external onlyOwner {
+        token.transfer(msg.sender, _amount);
+    }
+
+}
