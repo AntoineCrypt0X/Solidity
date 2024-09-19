78298f93f2be94761a91f8f149b52c1f4797f3d3
AntoineCrypt0X <antoine.poirier1995@gmail.com>
2024-09-19 16:26:40 +0200

    Update staking_fixedAmount.sol

--- Staking/staking_fixedAmount.sol
+++ Staking/staking_fixedAmount.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Staking1m is Ownable, ReentrancyGuard {
+contract Staking is Ownable, ReentrancyGuard {
     // ============= VARIABLES ============
 
     // Contract address of the staked token
