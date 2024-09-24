1ac600ec3813a5906d04cc1a93483b51d9c60838
AntoineCrypt0X <antoine.poirier1995@gmail.com>
2024-09-24 11:11:13 +0200

    Update NFT_referral_payable.sol

--- NFT/NFT_referral_payable.sol
+++ NFT/NFT_referral_payable.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// Referral = NFT holder. NFT price: 1 ether. Commission split 90%/10% between referral and contract owner
+// Referral = NFT holder. NFT price: 1 ether. Commission split between referral tree and contract owner
 contract NFT_referral_payable is Ownable, ERC721, ERC721Enumerable, ERC721URIStorage {
 
     using Counters for Counters.Counter;
@@ -15,6 +15,8 @@ contract NFT_referral_payable is Ownable, ERC721, ERC721Enumerable, ERC721URISto
 
     // NFT price
     uint256 public NFT_price;
+    // Number of levels of the referral tree
+    uint256 public max_levels_referral;
     // mapping user -> referral
     mapping ( address => address ) public  referral_users;
     // mapping user -> whitelist
@@ -25,6 +27,8 @@ contract NFT_referral_payable is Ownable, ERC721, ERC721Enumerable, ERC721URISto
     string public IPFS_URI;
     // mapping user -> NFT minted
     mapping(address=>uint256) public minted_Wallets;
+    // mapping level commission
+    mapping (uint256 => uint256) public commission_Levels;
 
     event Add_Whitelist(address[] listAddress);
     event Remove_Whitelist(address[] listAddress);
@@ -34,6 +38,10 @@ contract NFT_referral_payable is Ownable, ERC721, ERC721Enumerable, ERC721URISto
         _safeMint(msg.sender, _tokenIdCounter.current());
         _tokenIdCounter.increment();
         NFT_price = 1 ether; // 1 ether
+        max_levels_referral=3;
+        Levels[1]=50;
+        Levels[2]=25;
+        Levels[3]=15;
     }
 
     function add_whitelist(address[] memory listAddress) onlyOwner public returns (bool) {
@@ -52,6 +60,18 @@ contract NFT_referral_payable is Ownable, ERC721, ERC721Enumerable, ERC721URISto
         return true;
     }
 
+    function getChainParent(address user) public view returns(address[] memory){
+        uint256 count=1;
+        address[] memory listParents= new address[](uint256(max_levels_referral));
+        address Parent=referral_users[user];
+        while(count <= max_levels_referral && Parent != address(0)){
+            listParents[uint256(count-1)]=Parent;
+            Parent=referral_users[Parent];
+            count++;
+        }
+        return listParents;
+    }
+
     function safeMint(address input_referral, string memory username) payable public {
         require(msg.value== NFT_price,"wrong value sent");
         require(whitelist_address[msg.sender],"Address not whitelisted");
@@ -65,9 +85,25 @@ contract NFT_referral_payable is Ownable, ERC721, ERC721Enumerable, ERC721URISto
         username_dapp[msg.sender]=username;
         _setTokenURI(tokenId, IPFS_URI);
         _tokenIdCounter.increment();
-        // Commission split between referal and contract owner
-        payable(input_referral).transfer(msg.value*90/100);
-        payable(owner()).transfer(msg.value*10/100);
+        // Commission split between referrals and contract owner
+        uint256 commission_count=100;
+        uint256 level=1;
+        address Parent;
+        address[] memory parents=getNFTreferrals(input_referral);
+        if (parents.length>0){
+            Parent=parents[0];
+            while(level <= max_levels && Parent != address(0))
+            {
+                payable(Parent).transfer(msg.value*commission_Levels[level]/100);
+                commission_count-=Levels[level];
+                if (level <= max_levels-1){
+                    Parent=parents[level];
+                }
+                level++;
+            }
+        }
+        // the rest is sent to the contract owner
+        payable(owner()).transfer(msg.value*commission_count/100);
     }
 
     function modifyIPFS(string memory _ipfs) onlyOwner public returns (bool) {
@@ -88,12 +124,20 @@ contract NFT_referral_payable is Ownable, ERC721, ERC721Enumerable, ERC721URISto
         super._burn(tokenId);
     }
 
-    function tokenURI(uint256 tokenId) public view ride(ERC721, ERC721URIStorage) returns (string memory)
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
     {
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool)
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
     {
         return super.supportsInterface(interfaceId);
     }
