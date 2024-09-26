c046983053acb1200de6931f4cdccf447245d4cc
AntoineCrypt0X <antoine.poirier1995@gmail.com>
2024-09-26 12:20:44 +0200

    Update staking_penalty.sol

--- Staking/staking_penalty.sol
+++ Staking/staking_penalty.sol
@@ -23,6 +23,8 @@ contract StakingPenalty is Ownable, ReentrancyGuard {
     bool private active;
     // Fee collecting address from the "withdraw immediately"
     address public FEE_COLLECTING_WALLET;
+    // deposit rewards
+    uint public deposit_reward;
 
     // User address => start staking date
     mapping(address => uint) public userStartStakePeriod;
@@ -125,7 +127,16 @@ contract StakingPenalty is Ownable, ReentrancyGuard {
         stakingToken.transfer(msg.sender,_amount);
     }
 
+    function feedRewards(uint _amount) external onlyOwner {
+        require(_amount > 0, "amount must be greater than 0");
+        bool success = stakingToken.transferFrom(msg.sender,address(this),_amount);
+        require(success, "transfer was not successfull");
+        deposit_reward+=_amount;
+    }
+
     function return_To_Owner(uint256 _amount)  external onlyOwner {
+        require(deposit_reward>=_amount);
+        deposit_reward-=_amount;
         stakingToken.transfer(msg.sender, _amount);
     }
 
