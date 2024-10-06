351c2a29b84cafd14db41fc78c46a96feb7ab25d
AntoineCrypt0X <antoine.poirier1995@gmail.com>
2024-10-06 10:30:04 +0200

    Update token_deploy.sol

--- Tokens/token_deploy.sol
+++ Tokens/token_deploy.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+// Deploy a standard ERC20 token
 contract TokenERC20 is ERC20 {
 
     constructor(uint256 initialSupply,string memory _name, string memory _symbol) ERC20(_name, _symbol) {
